de5e1576915e0d4f310422a0a471c63a7f7afaf0
Park, Jimin <qoarhkdid@gmail.com>
2026-05-13 10:54:05 +0900

    five

--- src/e202014062/World.java
+++ src/e202014062/World.java
@@ -2,6 +2,11 @@ package e202014062;
 
 public class World {
     public static void run() {
-        System.out.println("202014062");
+        System.out.println("201514099"); // 본인 학번
+    }
+
+    // 아래 분홍색 부분 추가
+    public static void run2() {
+        System.out.println("hello world");
     }
 }
