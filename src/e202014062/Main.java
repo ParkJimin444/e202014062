3dbeef36e8b7931d6c33a8d74bbe60f0927a8d71
Park, Jimin <qoarhkdid@gmail.com>
2026-05-13 10:46:27 +0900

    three

--- src/e202014062/Main.java
+++ src/e202014062/Main.java
@@ -3,5 +3,6 @@ package e202014062;
 public class Main {
     public static void main(String[] args) {
         System.out.println("박지민");
+        World.run();
     }
 }
---
+++ src/e202014062/World.java
@@ -0,0 +1,7 @@
+package e202014062;
+
+public class World {
+    public static void run() {
+        System.out.println("202014062");
+    }
+}
